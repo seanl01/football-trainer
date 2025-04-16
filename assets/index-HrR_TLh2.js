baccc5c8638c4880c021e8c521d6d1c5da3deb94
seanl01 <57535001+seanl01@users.noreply.github.com>
2025-04-16 10:50:44 +0800

    Updates

---
+++ assets/index-HrR_TLh2.js
@@ -0,0 +1,16 @@
+import{j as e}from"./index-CwXlvHck.js";import{c,L as n}from"./createLucideIcon-BNYBayIM.js";/**
+ * @license lucide-react v0.483.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["circle",{cx:"18",cy:"18",r:"3",key:"1xkwt0"}],["circle",{cx:"6",cy:"6",r:"3",key:"1lh9wr"}],["path",{d:"M13 6h3a2 2 0 0 1 2 2v7",key:"1yeb86"}],["path",{d:"M11 18H8a2 2 0 0 1-2-2V9",key:"19pyzm"}]],i=c("GitCompare",r);/**
+ * @license lucide-react v0.483.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"M9.3 6.2a4.55 4.55 0 0 0 5.4 0",key:"flyxqv"}],["path",{d:"M7.9 10.7c.9.8 2.4 1.3 4.1 1.3s3.2-.5 4.1-1.3",key:"1nlxxg"}],["path",{d:"M13.9 3.5a1.93 1.93 0 0 0-3.8-.1l-3 10c-.1.2-.1.4-.1.6 0 1.7 2.2 3 5 3s5-1.3 5-3c0-.2 0-.4-.1-.5Z",key:"vz7x1l"}],["path",{d:"m7.5 12.2-4.7 2.7c-.5.3-.8.7-.8 1.1s.3.8.8 1.1l7.6 4.5c.9.5 2.1.5 3 0l7.6-4.5c.7-.3 1-.7 1-1.1s-.3-.8-.8-1.1l-4.7-2.8",key:"1xfzlw"}]],s=c("TrafficCone",l);/**
+ * @license lucide-react v0.483.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["circle",{cx:"12",cy:"8",r:"5",key:"1hypcn"}],["path",{d:"M20 21a8 8 0 0 0-16 0",key:"rfgkzh"}]],p=c("UserRound",d),y=function(){const a=[{title:"Paired Reaction Trainer",link:"/football-trainer/pair",icon:e.jsx(i,{className:"drop-shadow-md w-8 h-8"})},{title:"Individual Reaction Trainer",link:"/football-trainer/trainer",icon:e.jsx(p,{className:"drop-shadow-md w-8 h-8"})},{title:"Dot Drill",link:"/football-trainer/dotDrill",icon:e.jsx(s,{className:"drop-shadow-md w-8 h-8"})}];return e.jsxs(e.Fragment,{children:[e.jsx("h1",{className:"text-xl font-bold grid place-self-center text-center mb-4",children:"Football Trainer"}),e.jsx("section",{className:"grid grid-cols-1 gap-3 w-full pt-4",children:a.map((t,o)=>e.jsxs(n,{to:t.link,className:"btn btn-primary rounded-lg min-h-48 relative",children:[e.jsx("section",{className:"",children:e.jsx("h2",{className:"text-2xl",children:t.title})}),e.jsx("section",{className:"absolute bottom-5 right-5",children:t.icon})]},o))})]})};export{y as component};
